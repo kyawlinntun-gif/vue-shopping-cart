dea1b5e8f57b6b766e25680c41939df8f3ebb985
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2021-05-31 10:35:58 +0630

    Initialized project setup, create nav bar, Home and Cart components, and also create items js file to import data to Home component

---
+++ src/views/Cart.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>This is the cart page.</div>
+</template>
+
+<script>
+
+export default {
+    name: 'Cart'
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="home">
+
+        <div class="product-cards-container">
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import items from '../data/items.js';
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            items: items
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .product-cards-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>
