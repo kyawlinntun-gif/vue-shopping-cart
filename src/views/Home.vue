e574f58f7cd132de3c3648aa51b36e3f86e1bf8b
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2021-06-01 12:35:03 +0630

    Create ProductSummaryCart components and import data for view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="home">
 
         <div class="product-cards-container">
-
+            <ProductSummaryCart v-for="product in items" v-bind:key="product.id" :product="product" v-on:view-product="viewProduct($event)" />
         </div>
 
     </div>
@@ -11,12 +11,22 @@
 <script>
 // @ is an alias to /src
 import items from '../data/items.js';
+import ProductSummaryCart from '../components/products/ProductSummaryCard.vue';
 
 export default {
     name: 'Home',
+    components: {
+        ProductSummaryCart
+    },
     data() {
         return {
             items: items
+        }
+    },
+    methods: {
+        viewProduct($event)
+        {
+            console.log($event);
         }
     }
 }
